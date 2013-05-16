a44641178442541cac91013dc27dac1ecfb35a62
Siva Kumar <sivakumar.tangudu@chronus.com>
2013-05-16 14:43:22 +0530

    misc changes

--- app/controllers/plan_creator_controller.rb
+++ app/controllers/plan_creator_controller.rb
@@ -4,6 +4,7 @@ class PlanCreatorController < ApplicationController
   steps :select_template, :edit_tasks, :assign_contacts
 
   def show
+    @plan = Plan.find(1)
     @task_templates = TaskTemplate.find(:all)
     render_wizard
   end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -17,6 +17,7 @@
 #
 
 class Plan < ActiveRecord::Base
+  attr_accessor :selected_task_templates
   attr_accessible :bu_id, :creator_id, :end_date, :hr_id, :manager_id, :mentor_id, :start_date, :title, :user_id
 
   has_many :task
